cc860b811811e4b3088f01499b5de42d169374f9
RatsovPhilip <ratsovphilip@gmail.com>
2019-07-23 21:34:22 +0300

    Added createViewModel class to github

---
+++ TaskPlanner/Models/CompanyCreateViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TaskPlanner.Models
+{
+    public class CompanyCreateViewModel
+    {
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+    }
+}
