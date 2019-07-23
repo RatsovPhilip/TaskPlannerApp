e279c69d362cdabbc2b8c5b5a4e3a9f132990732
RatsovPhilip <ratsovphilip@gmail.com>
2019-07-23 21:35:26 +0300

    Added Company/Create.cshtml and FirstLogIn/Create.cshtml to github

---
+++ TaskPlanner.Service/GlobalConstants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskPlanner.Service
+{
+    public abstract class GlobalConstants
+    {
+        private const string companyNameDuplicate = "The compnay name is already taken. Try with another one.";
+    }
+}
