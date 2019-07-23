3328d0cc8ebe293b1b2487e105dbd29ebfcd5cde
RatsovPhilip <ratsovphilip@gmail.com>
2019-07-23 21:33:28 +0300

    Added CompanyController + FirstLogInController to github

---
+++ TaskPlanner/Controllers/CompanyController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using TaskPlanner.Data.Models;
+using TaskPlanner.Models;
+using TaskPlanner.Service;
+
+namespace TaskPlanner.Controllers
+{
+    public class CompanyController : Controller
+    {
+        private readonly ICompanyService companyService;
+        private readonly UserManager<ApplicationUser> userManager;
+
+        public CompanyController(ICompanyService companyService, UserManager<ApplicationUser> userManager)
+        {
+            this.companyService = companyService;
+            this.userManager = userManager;
+        }
+        public IActionResult Create()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(CompanyCreateViewModel companyModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var userId = this.userManager.GetUserId(this.User);
+
+                var company = new Company
+                {
+                    Name = companyModel.Name
+                  
+                };
+
+                this.companyService.CreateCompany(company);
+            }
+
+            
+            return Redirect("/");
+        }
+
+    }
+}
---
+++ TaskPlanner/Controllers/FirstLogInController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TaskPlanner.Controllers
+{
+    public class FirstLogInController:Controller
+    {
+        public IActionResult Create()
+        {
+            return this.View();
+        }
+    }
+}
