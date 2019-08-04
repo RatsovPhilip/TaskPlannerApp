a2f064ce980ffd4c4ac837bfa8573f817f790cb0
RatsovPhilip <ratsovphilip@gmail.com>
2019-08-04 17:36:05 +0300

    Added DailyAgendaViewModel.cs

---
+++ TaskPlanner/Models/DailyAgendaCreateViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TaskPlanner.Models
+{
+    public class DailyAgendaCreateViewModel
+    {
+        public string Id { get; set; }
+
+        public string Subject { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public string ThemeColor { get; set; }
+
+    }
+}
